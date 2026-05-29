508deb7ab3b305c1908edfa43a35912d42ca72ac
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-05-29 15:01:14 +0800

    Update app-sidebar.tsx
    added padding

--- src/features/products/components/app-sidebar.tsx
+++ src/features/products/components/app-sidebar.tsx
@@ -48,7 +48,7 @@ export function AppSidebar() {
                   <img
                     src="/icon.png"
                     alt="API Test"
-                    className="h-full w-full"
+                    className="h-full w-full object-contain p-1"
                   />
                 </div>
 
