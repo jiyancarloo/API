be2c9047c586b14ac3b5f207a7a2844ca44be9fa
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-06-05 16:27:10 +0800

    Update Dashboard.tsx
    Re layout dashboard to create a bento style grid to enhance user experience

--- src/features/products/pages/Dashboard.tsx
+++ src/features/products/pages/Dashboard.tsx
@@ -1,9 +1,32 @@
 import StatCards from "@/components/Dashboard/stat-card"
+import InventoryHealth from "@/components/Dashboard/inventory-health"
+import CategoryChart from "@/components/Dashboard/category-chart"
+import RecentProducts from "@/components/Dashboard/recent-products"
+import LowProductStats from "@/components/Dashboard/lowstock-products"
 export default function Dashboard() {
   return (
     <>
-      <div className="flex flex-col gap-10">
-        <StatCards />
+      <div className="flex flex-col gap-6">
+        <section className="grid gap-3 sm:grid-cols-1 lg:grid-cols-4">
+          <StatCards />
+        </section>
+
+        <section className="grid gap-4 lg:grid-cols-3">
+          <div>
+            <InventoryHealth />
+          </div>
+          <div className="row-span-2 lg:col-span-2">
+            <LowProductStats />
+          </div>
+          <div>
+            <RecentProducts />
+          </div>
+        </section>
+
+        <section className="grid">
+          {" "}
+          <CategoryChart />
+        </section>
       </div>
     </>
   )
