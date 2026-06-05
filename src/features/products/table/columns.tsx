e9a7f4d076ba0f3f4caf31ba57f04615883ce714
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-06-05 16:26:24 +0800

    Update columns.tsx
    Added actions Column which holds edit and delete buttons

--- src/features/products/table/columns.tsx
+++ src/features/products/table/columns.tsx
@@ -1,21 +1,28 @@
 import type { ColumnDef } from "@tanstack/react-table"
 import type { Product, ProductParams } from "../types/product.types"
 import { Badge } from "@/components/ui/badge"
-import { TriangleAlert, CircleCheck } from "lucide-react"
+import { TriangleAlert, CircleCheck, MoreHorizontal } from "lucide-react"
 import { ColumnHeader } from "./column-header"
 import { HeaderDrawer } from "./header-drawer"
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu"
 
 export const getColumns = (
   params: ProductParams,
-
-  setParams: React.Dispatch<React.SetStateAction<ProductParams>>
+  setParams: React.Dispatch<React.SetStateAction<ProductParams>>,
+  onEdit: (product: Product) => void,
+  onDelete: (product: Product) => void
 ): ColumnDef<Product>[] => [
   {
     accessorKey: "title",
     header: () => (
       <ColumnHeader
-        title="Name"
+        title="Product"
         sortKey="title"
         params={params}
         setParams={setParams}
@@ -74,6 +81,43 @@ export const getColumns = (
               </>
             )}
           </Badge>
+        </>
+      )
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const product = row.original
+
+      return (
+        <>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <MoreHorizontal className="size-4" />
+              </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem
+                onClick={() => {
+                  onEdit(product)
+                }}
+              >
+                Edit
+              </DropdownMenuItem>
+
+              <DropdownMenuItem
+                variant="destructive"
+                onClick={() => {
+                  onDelete(product)
+                }}
+              >
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </>
       )
     },
