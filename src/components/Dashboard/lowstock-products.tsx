ced45101a4d773e9458464ad61cb9e506dea9421
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-06-09 15:49:29 +0800

    Update lowstock-products.tsx
    added a scrollable component to allow scrolling in the container

--- src/components/Dashboard/lowstock-products.tsx
+++ src/components/Dashboard/lowstock-products.tsx
@@ -16,6 +16,7 @@ import {
 import { Badge } from "../ui/badge"
 import { TriangleAlert, Info } from "lucide-react"
 import { Button } from "../ui/button"
+import { ScrollArea } from "@/components/ui/scroll-area"
 export default function LowProductStats() {
   const { lowStockProducts } = useDashboardStats()
   return (
@@ -28,27 +29,38 @@ export default function LowProductStats() {
           </Button>
         </CardAction>
       </CardHeader>
-      <CardContent className="space-y-3">
-        {lowStockProducts.map((low) => (
-          <Item variant="outline" key={low.id} size="sm">
-            <ItemMedia variant="icon">
-              {low.severity === "critical" ? (
-                <TriangleAlert className="h-14 w-14" />
-              ) : (
-                <Info className="h-14 w-14" />
-              )}
-            </ItemMedia>
-            <ItemContent className="line-clamp-1">
-              <ItemTitle>{low.title}</ItemTitle>
-              <ItemDescription>
-                {low.severity === "critical"
-                  ? "Need's urgent attenion"
-                  : "Monitor and address soon"}
-              </ItemDescription>
-            </ItemContent>
-            <Badge variant="destructive">{low.stock} stocks</Badge>
-          </Item>
-        ))}
+
+      <CardContent>
+        <ScrollArea className="h-117.5">
+          <div className="space-y-3 pr-4">
+            {lowStockProducts.map((low) => (
+              <Item variant="outline" key={low.id} size="sm">
+                <ItemMedia variant="icon">
+                  {low.severity === "critical" ? (
+                    <TriangleAlert className="h-14 w-14" />
+                  ) : (
+                    <Info className="h-14 w-14" />
+                  )}
+                </ItemMedia>
+
+                <ItemContent className="line-clamp-1">
+                  <ItemTitle>
+                    {low.title} {"-"}
+                    <span className="text-muted-foreground">
+                      {low.severity === "critical"
+                        ? "Needs urgent attention"
+                        : "Monitor and address soon"}
+                    </span>
+                  </ItemTitle>
+
+                  <ItemDescription>{low.category}</ItemDescription>
+                </ItemContent>
+
+                <Badge variant="destructive">{low.stock} stocks</Badge>
+              </Item>
+            ))}
+          </div>
+        </ScrollArea>
       </CardContent>
     </Card>
   )
