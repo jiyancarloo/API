bae3bb66df80a605d92457daac8d2cdb8a681e67
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-06-09 15:51:02 +0800

    Update useProduct.ts
    adjusted limit number of products to display

--- src/features/products/hooks/useProduct.ts
+++ src/features/products/hooks/useProduct.ts
@@ -6,7 +6,7 @@ import { useDebounce } from "./useDebounce"
 
 export function useProduct() {
   const [params, setParams] = useState<ProductParams>({
-    limit: 10,
+    limit: 13,
     skip: 0,
     sortBy: "title",
     order: "asc",
