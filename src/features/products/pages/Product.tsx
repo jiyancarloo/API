d768171c01270c73b2271c7cd6bf7c7358ae6b84
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-06-09 15:48:49 +0800

    Update Product.tsx

--- src/features/products/pages/Product.tsx
+++ src/features/products/pages/Product.tsx
@@ -2,7 +2,7 @@ import TableView from "@/components/Dashboard/product-table"
 export default function ViewProducts() {
   return (
     <>
-      <div>
+      <div className="">
         <TableView />
       </div>
     </>
