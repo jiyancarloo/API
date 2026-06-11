878c2454c3f2326665a1f22b92a75b95f1999e72
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-06-11 08:20:31 +0800

    Update product.utils.ts
    added an and and custom styling depending on status of inventory

--- src/features/products/utils/product.utils.ts
+++ src/features/products/utils/product.utils.ts
@@ -1,5 +1,5 @@
 import type { Product } from "../types/product.types"
-
+import { TrendingUp, TrendingDown, TriangleAlert } from "lucide-react"
 export function getDashboardStats(products: Product[]) {
   const totalProducts = products.length
   const totalValue = products.reduce((sum, p) => sum + p.price, 0)
@@ -17,23 +17,33 @@ export function getProductInventoryStatus(stock: number) {
   if (stock === 0)
     return {
       label: "Out of Stock",
-      variant: "destructive",
+      icon: TriangleAlert,
+      className: "bg-red-100 text-red-700 border-red-200",
+      description:
+        "This product is unavailable and requires immediate restocking.",
     }
 
   if (stock <= 5)
     return {
       label: "Critical Stock",
-      variant: "destructive",
+      icon: TrendingDown,
+      className: "bg-orange-100 text-orange-700 border-orange-200",
+      description:
+        "Inventory is critically low. Replenishment is recommended immediately.",
     }
 
   if (stock < 20)
     return {
       label: "Low Stock",
-      variant: "secondary",
+      icon: TrendingDown,
+      className: "bg-yellow-100 text-yellow-700 border-yellow-200",
+      description: "Stock levels are running low. Monitor inventory closely.",
     }
 
   return {
     label: "Healthy Stock",
-    variant: "default",
+    icon: TrendingUp,
+    className: "bg-green-100 text-green-700 border-green-200",
+    description: "Inventory levels are healthy and sufficient for demand.",
   }
 }
